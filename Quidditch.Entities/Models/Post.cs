2e6185ee3ee966159ee87610bdadd922df6b0fcc
Loes Reyniers <490511@student.fontys.nl>
2022-05-31 15:34:36 +0200

    Comments toegevoegd om naar te kijken

--- Quidditch.Entities/Models/Post.cs
+++ Quidditch.Entities/Models/Post.cs
@@ -8,7 +8,7 @@ namespace Quidditch.Entities.Models
     public class Post
     {
         public int Id { get; set; }
-        public int UserId { get; set; }
+        public int UserId { get; set; } // User ID niet hier zetten, maar lijst van post maken in user
         public string Titel { get; set; }
         public string Body { get; set; }
     }
